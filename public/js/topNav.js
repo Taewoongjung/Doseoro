66c1f42fd0f1d20dae00b7701b2600782e85ad55
Bhanjo <hanjo1515@naver.com>
2021-03-24 02:12:42 +0900

    03_24(프론트)

---
+++ public/js/topNav.js
@@ -0,0 +1,6 @@
+function setInnerHTML() {
+    const navInner = document.getElementById('topNav');
+    navInner.innerHTML = '<div class="container-fluid"><a class="navbar-brand" href="#"><img src="img\logo3.png" width="200" height="80"></a> <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span></button><div class="collapse navbar-collapse justify-content-sm-end" id="navbarCollapse"><ul class="navbar-nav mb-2 mb-md-0"><li class="nav-item"><a class="nav-link p-3" href="#">고객문의</a></li><li class="nav-item"><a class="nav-link p-3" href="#">관심상품</a></li><li class="nav-item"><a class="nav-link p-3" href="#">로그인</a></li><li class="nav-item"><a class="nav-link p-3" href="#">마이페이지</a></li></ul></div></div>';
+    navInner.innerHTML = '배한조';
+    console.log('실행');
+}
