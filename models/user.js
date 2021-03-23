739621acae9854ba2605c07a7dfcff550cc414b6
Taewoong <aipooh8882@naver.com>
2021-03-24 03:56:35 +0900

    query

---
+++ models/user.js
@@ -0,0 +1,39 @@
+const Sequelize = require('sequelize');
+
+module.exports = class User extends Sequelize.Model {
+    static init(sequelize) {
+        return super.init({
+            email: {
+                type: Sequelize.STRING(40),
+                allowNull: false,
+                unique: true,
+            },
+            nick: {
+                type: Sequelize.STRING(15),
+                allowNull: false,
+            },
+            password: {
+                type: Sequelize.STRING(100),
+                allowNull: false,
+            },
+            phone: {
+                type: Sequelize.STRING(11),
+                allowNull: false,
+                defaultValue: 0,
+            },
+        }, {
+            sequelize,
+            timestamps: true,
+            paranoid: true,
+            modelName: 'User',
+            tableName: 'users',
+            charset: 'utf8',
+            collate: 'utf8_general_ci',
+        });
+    }
+
+    // static associate(db) {
+    //     db.User.hasMany(db.);
+    // }
+};
+
