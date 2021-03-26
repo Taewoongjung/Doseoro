ddc7b854789e51f905da4c8b80531472e459fc9d
Taewoong <aipooh8882@naver.com>
2021-03-26 14:31:41 +0900

    kakao

--- passport/kakao.js
+++ passport/kakao.js
@@ -7,12 +7,22 @@ module.exports = () => {
     passport.use(new KakaoStrategy({
         clientID: process.env.KAKAO_ID,
         callbackURL: '/auth/kakao/callback',
-    }, async (accessToken, refreshToken, profile, done) => {
+    }, async (accessToken, refreshToken, profile, done) => {  // 이 서비스에서는 카카오 profile만 받아옴
         console.log('kakao profile', profile);
         try {
             const exUser = await User.findOne({
                 where: { snsId: profile.id, provider: 'kakao' },
-            })
+            });
+            if (exUser) {
+                done(null, exUser);
+            } else {
+                const newUser = await User.create({
+                    email.: profile._json && profile._json.kakao_account_email,
+                    nick: profile.displayName,
+                    snsId: profile.id,
+                    provider: 'kakao',
+                })
+            }
         }
     }))
 };
