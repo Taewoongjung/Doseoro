058fac4674bfdb30f0f5bb38b0865b1adb22c808
Bhanjo <hanjo1515@naver.com>
2021-03-30 01:14:59 +0900

    0329_check tel, pwd format

---
+++ public/js/checkFormat.js
@@ -0,0 +1,23 @@
+//회원가입 페이지 비밀번호 일치여부 판단
+function checkPwd() {   
+    const userPassword = document.getElementById('userPassword');
+    const userPasswordCheck = document.getElementById('userPasswordCheck');
+    const pwdCheck = document.getElementById('pwdCheck');
+     
+    if (userPassword.value != userPasswordCheck.value) {
+        pwdCheck.innerHTML = "<a>비밀번호 불일치</a>";
+    } else if (userPassword.value == userPasswordCheck.value) {
+        pwdCheck.innerHTML = '<a>비밀번호 일치</a>';
+    } else {
+        alert('에러!');
+    }
+}
+
+//회원가입 페이지 핸드폰 번호 자동 '-' 입력
+function telFormat(str) {
+    let inputPhone = document.getElementById('inputPhone');
+    inputPhone = inputPhone.value.split('-').join('');
+
+    let lastNum = inputPhone.replace(/(\d)(\d)(\d)(\d)(\d)(\d)(\d)(\d)(\d)(\d)(\d)/, '$1$2$3-$4$5$6$7-$8$9$10$11');
+    document.getElementById('inputPhone').value = lastNum;
+}
