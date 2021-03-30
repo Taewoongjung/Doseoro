0d0da44da5e128ac520329501b09598e8cf4741b
Taewoong <aipooh8882@naver.com>
2021-03-31 01:18:46 +0900

    ready for uploading images

--- app.js
+++ app.js
@@ -48,6 +48,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(morgan('dev'));
 }
 app.use(express.static(path.join(__dirname, './public')));
+app.use('/img', express.static(path.join(__dirname, 'uploads')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(favicon(path.join(__dirname, '/public/img/logo_clear.png')));
