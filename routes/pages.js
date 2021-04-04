232d15a454ecc3808bcedc6218b826e946116e39
Taewoong <aipooh8882@naver.com>
2021-04-04 22:19:40 +0900

    saleBoard

--- routes/pages.js
+++ routes/pages.js
@@ -3,6 +3,11 @@ const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
 
 const router = express.Router();
 
+router.use((req, res, next) => { // 모든 라우터에 회원정보 넣어주기
+    res.locals.user = req.user;
+    next();
+});
+
 router.get('/regi-book', isLoggedIn, (req, res) => {
     res.render('registerBook.html');
 })
@@ -17,6 +22,10 @@ router.get('/findPW', isNotLoggedIn,  (req, res) => {
 
 router.get('/changePW', isNotLoggedIn, (req, res) => {
     res.render('changePW.html');
+})
+
+router.get('/saleBoard', (req, res) => {
+    res.render('saleBoard.html');
 })
 
 module.exports = router;
