483ea3ea5b1a62cc46e9e7bb4fc3fa514181f824
Taewoong <aipooh8882@naver.com>
2021-04-10 17:23:25 +0900

    timezone added

--- config/config.js
+++ config/config.js
@@ -6,20 +6,38 @@ module.exports = {
     "password": process.env.DB_SECRET,
     "database": "Teamproject",
     "host": "127.0.0.1",
-    "dialect": "mysql"
+    "dialect": "mysql",
+    "timezone": '+09:00',
+    'pool': {
+      max: 20,   // 최대 유지 connection 수
+      min: 5,    // 최소 유지 connection 수
+      idle: 60000 // connection을 몇ms까지 대기시킬 것인가 (이후엔 버려짐)
+    }
   },
   "test": {
     "username": "root",
     "password": process.env.DB_SECRET,
     "database": "Teamproject",
     "host": "127.0.0.1",
-    "dialect": "mysql"
+    "dialect": "mysql",
+    "timezone": '+09:00',
+    'pool': {
+      max: 20,  
+      min: 5,  
+      idle: 60000 
+    }
   },
   "production": {
     "username": "root",
     "password": process.env.DB_SECRET,
     "database": "Teamproject",
     "host": "127.0.0.1",
-    "dialect": "mysql"
+    "dialect": "mysql",
+    "timezone": '+09:00',
+    'pool': {
+      max: 20, 
+      min: 5,  
+      idle: 60000 
+    }
   }
 }
