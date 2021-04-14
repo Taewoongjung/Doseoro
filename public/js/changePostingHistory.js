ae3754de80d7871f39f12c138b154cede94faa16
Bhanjo <hanjo1515@naver.com>
2021-04-14 21:43:49 +0900

    0414_posting list page

---
+++ public/js/changePostingHistory.js
@@ -0,0 +1,32 @@
+const salePosting = document.getElementById('salePosting');
+const requestPosting = document.getElementById('requestPosting');
+const donatePosting = document.getElementById('donatePosting');
+const communityPosting = document.getElementById('communityPosting');
+
+function showSale() {
+    console.log('판매클릭');
+
+    requestPosting.style.display = donatePosting.style.display = communityPosting.style.display = "none";
+    salePosting.style.display = "block";
+}
+
+function showRequset() {
+    console.log('구매클릭');
+
+    salePosting.style.display = donatePosting.style.display = communityPosting.style.display = "none";
+    requestPosting.style.display = "block";
+}
+
+function showDonate() {
+    console.log('나눔클릭');
+
+    requestPosting.style.display = salePosting.style.display = communityPosting.style.display = "none";
+    donatePosting.style.display = "block";
+}
+
+function showCommunity() {
+    console.log('커뮤클릭');
+
+    requestPosting.style.display = donatePosting.style.display = salePosting.style.display = "none";
+    communityPosting.style.display = "block";
+}
