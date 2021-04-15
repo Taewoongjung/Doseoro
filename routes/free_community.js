a46e92c3f18322d970e733afee8c63761fa542c7
Taewoong <aipooh8882@naver.com>
2021-04-15 15:26:17 +0900

    edit/delete functions on free distribution

--- routes/free_community.js
+++ routes/free_community.js
@@ -52,5 +52,57 @@ router.post('/book', isLoggedIn, upload.single('img'), async (req, res, next) =>
     }
 });
 
+// 0415 무료나눔내역 삭제
+router.get('/delete', isLoggedIn, async (req, res, next) => {
+    try {
+        const { this_item_id, this_item_createdAt, this_item_OwnerId } = req.query;
+        await Book.destroy({ where: { id: this_item_id, createdAt: this_item_createdAt, OwnerId: this_item_OwnerId }, });
+        res.send(`<script type="text/javascript">alert("게시물 삭제 완료!"); location.href="/pages/myPostingList";</script>`);
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
+});
+
+// 0415 무료나눔 창에 수정을 누르면 나오는 수정하는 창을 띄어주는 라우터
+router.post('/editIt', isLoggedIn, async (req, res, next) => {
+    try {
+        const { this_item_id } = req.body;
+        const books = await Book.findOne({ where: { id: this_item_id, price: -1 } });
+        console.log("books = ", books);
+        res.render('edit_freeDetail.html', {
+            books,
+        });
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
+});
+
+// 0415 무료나눔요청 게시물 수정하기
+router.post('/edit', isLoggedIn, async (req, res, next) => {
+    try {
+        const { this_item_id, postmessage, title, author, publisher, checkCategory, dealRoot, about } = req.body;
+        console.log("body = ", req.body);
+        const a = await Book.update({
+            postmessage: postmessage,
+            title: title,
+            author: author,
+            publisher: publisher,
+            category: checkCategory,
+            tradingmethod: dealRoot,
+            about: about,
+        }, {
+            where: { id: this_item_id, price: -1 }
+        });
+        console.log("body = ", req.body);
+        console.log("aa = ", a);
+
+        res.send(`<script type="text/javascript">alert("구매하기 정보 수정 완료"); location.href="/book/${this_item_id}";</script>`); // 등록 하고 자기가 등록한 책 화면 띄우게 하기
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
+});
 
 module.exports = router;
