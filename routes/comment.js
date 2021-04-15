d42a1270f66341dd56be72d4bcd767c6bc7615e1
Taewoong <aipooh8882@naver.com>
2021-04-15 21:50:37 +0900

    2021-04-15

--- routes/comment.js
+++ routes/comment.js
@@ -1,11 +1,8 @@
 const express = require('express');
-const multer = require('multer');
-const path = require('path');
-const fs = require('fs');
 const sequelize = require("sequelize");
 const Op = sequelize.Op;
 
-const { User, Book, Who, Post } = require('../models');
+const { User, Book, Who, Post, Community } = require('../models');
 const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
 
 const router = express.Router();
@@ -39,6 +36,23 @@ router.get('/commentEdit', isLoggedIn, async (req, res, next) => {
       }
 });
 
+// 댓글 삭제 (판매)
+router.get('/commentDelete', isLoggedIn, async (req, res, next) => {
+    try {
+        const { UserId, commentId, comment_createdAt, bookId } = req.query;
+        const thisBook = await Book.findOne({ where: { id: bookId } });
+        if (UserId === String(res.locals.user.id)){
+            await Post.destroy({ where: { id: commentId, UserId: req.user.id, createdAt: comment_createdAt } });
+
+            return res.send(`<script type="text/javascript">alert("댓글이 삭제 되었습니다!"); location.href="/book/${thisBook.id}";</script>`);        
+        } else {
+            return res.send(`<script type="text/javascript">alert("삭제 권한이 없습니다!"); location.href="/book/${thisBook.id}";</script>`);  
+        }} catch (err) {
+        console.error(err);
+        next(err);
+      }
+});
+
 // 0403 댓글 수정(구매)
 router.get('/commentEdit_buy', isLoggedIn, async (req, res, next) => {
     try {
@@ -63,23 +77,6 @@ router.get('/commentEdit_buy', isLoggedIn, async (req, res, next) => {
       }
 });
 
-// 댓글 삭제 (판매)
-router.get('/commentDelete', isLoggedIn, async (req, res, next) => {
-    try {
-        const { UserId, commentId, comment_createdAt, bookId } = req.query;
-        const thisBook = await Book.findOne({ where: { id: bookId } });
-        if (UserId === String(res.locals.user.id)){
-            await Post.destroy({ where: { id: commentId, UserId: req.user.id, createdAt: comment_createdAt } });
-
-            return res.send(`<script type="text/javascript">alert("댓글이 삭제 되었습니다!"); location.href="/book/${thisBook.id}";</script>`);        
-        } else {
-            return res.send(`<script type="text/javascript">alert("삭제 권한이 없습니다!"); location.href="/book/${thisBook.id}";</script>`);  
-        }} catch (err) {
-        console.error(err);
-        next(err);
-      }
-});
-
 // 댓글 삭제 (구매)
 router.get('/commentDelete_buy', isLoggedIn, async (req, res, next) => {
     try {
@@ -91,6 +88,49 @@ router.get('/commentDelete_buy', isLoggedIn, async (req, res, next) => {
             return res.send(`<script type="text/javascript">alert("댓글이 삭제 되었습니다!"); location.href="/wannabuy/buybook/${thisBook.id}";</script>`);        
         } else {
             return res.send(`<script type="text/javascript">alert("삭제 권한이 없습니다!"); location.href="/wannabuy/buybook/${thisBook.id}";</script>`);  
+        }} catch (err) {
+        console.error(err);
+        next(err);
+      }
+});
+
+
+// 0415 댓글 수정(커뮤니티)
+router.get('/commentEdit_commu', isLoggedIn, async (req, res, next) => {
+    try {
+        const { UserId, commentId, comment_createdAt, communityId, edited_comment } = req.query;
+        console.log("Com = ", edited_comment);
+        console.log("Community id = ", communityId);
+        const thisCommunity = await Community.findOne({ where: { id: String(communityId) } });
+        if (UserId === String(res.locals.user.id)){
+
+            await Post.update({ 
+                content: edited_comment,
+            }, {
+                where: { id: commentId, UserId: req.user.id, createdAt: comment_createdAt } 
+            });
+            
+            return res.send(`<script type="text/javascript">alert("댓글이 수정 되었습니다!"); location.href="/free_community/community/${thisCommunity.postingId}";</script>`);   
+
+        } else {
+            return res.send(`<script type="text/javascript">alert("수정 권한이 없습니다!"); location.href="/wannabuy/buybook/${thisCommunity.postingId}";</script>`);  
+        }} catch (err) {
+        console.error(err);
+        next(err);
+      }
+});
+
+// 0415 댓글 삭제 (커뮤니티)
+router.get('/commentDelete_commu', isLoggedIn, async (req, res, next) => {
+    try {
+        const { UserId, commentId, comment_createdAt, communityId } = req.query;
+        const thisCommunity = await Community.findOne({ where: { id: communityId } });
+        if (UserId === String(res.locals.user.id)){
+            await Post.destroy({ where: { id: commentId, UserId: req.user.id, createdAt: comment_createdAt } });
+
+            return res.send(`<script type="text/javascript">alert("댓글이 삭제 되었습니다!"); location.href="/wannabuy/buybook/${thisCommunity.postingId}";</script>`);        
+        } else {
+            return res.send(`<script type="text/javascript">alert("삭제 권한이 없습니다!"); location.href="/wannabuy/buybook/${thisCommunity.postingId}";</script>`);  
         }} catch (err) {
         console.error(err);
         next(err);
