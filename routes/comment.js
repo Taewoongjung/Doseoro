8b87e442aa1d00b5091869cfdd7a6d0c490c2226
Taewoong <aipooh8882@naver.com>
2021-04-22 22:34:00 +0900

    2021-04-22

--- routes/comment.js
+++ routes/comment.js
@@ -55,21 +55,26 @@ router.get('/commentDelete', isLoggedIn, async (req, res, next) => {
 });
 
 // 대댓글 수정(판매)
-router.get('/commentEdit', isLoggedIn, async (req, res, next) => {
+router.get('/reCommentEdit', isLoggedIn, async (req, res, next) => {
     try {
-        const { UserId, commentId, bookId, edited_comment } = req.query;
-        console.log("Com = ", edited_comment);
+        const { recomment_UserId, bookId, commentId, reCom_edited_comment, recomment_reCommentedId } = req.query;
+        console.log("Com = ", reCom_edited_comment);
+        console.log("ID = ", commentId);
+        console.log("recomment_reCommentedId = ", recomment_reCommentedId);
+        console.log("UserId = ", String(recomment_UserId));
+        console.log("req.user.id = ", req.user.id);
+        console.log("req.locals.user.id = ", res.locals.user.id);
         const thisBook = await Book.findOne({ where: { id: bookId } });
-        if (UserId !== String(res.locals.user.id)){
+        if (recomment_UserId !== String(req.user.id)){
             return res.send(`<script type="text/javascript">alert("수정 권한이 없습니다!"); location.href="/book/${thisBook.id}";</script>`);  
         } else {
-            if ( edited_comment === String(null)){
+            if ( reCom_edited_comment === String(null)){
                 return res.send(`<script type="text/javascript">alert("댓글이 수정이 취소 되었습니다!"); location.href="/book/${thisBook.id}";</script>`);   
             }
-            await Post.update({ 
-                content: edited_comment,
+            await Post.update({
+                content: reCom_edited_comment,
             }, {
-                where: { id: commentId, UserId: req.user.id } 
+                where: { id: commentId } 
             });
             return res.send(`<script type="text/javascript">alert("댓글이 수정 되었습니다!"); location.href="/book/${thisBook.id}";</script>`);
         }
