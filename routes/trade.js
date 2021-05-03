7da2b6d4b2eb28455eb1257509e61fe2ff80616a
Taewoong <aipooh8882@naver.com>
2021-05-04 02:32:27 +0900

    자신에게 거래 요청 못하게, 판매자가 아닌 사람 구별 처리

--- routes/trade.js
+++ routes/trade.js
@@ -22,15 +22,27 @@ router.post('/commentTrade', isLoggedIn, async (req, res, next) => {
     console.log("책 아이디 = ", bookId);
     console.log("책 유저 아이디 = ", UserId);
 
+    const theComment = await Post.findOne({
+        where:{
+            UserId: UserId,
+        }
+    });
+    console.log("댓글 유저 아이디 = ", theComment.id);
+    console.log("댓글 유저 닉네임 = ", theComment.commentingNick);
+
     if (req.user.id === theBook.OwnerId) {
         console.log("통과");
-        await Book.update({  // 거래 완료. 거래 완료 되면 화면에 뿌려주는 아이템들의 조건들이 다 바뀔 예정
-            sold: 1,
-            SoldId: UserId,
-        }, {
-            where: { id: theBook.id},
-        });                             // 거래 완료되면 판매내역으로 이동할지 조원들과 회의
-        return res.send(`<script type="text/javascript">alert("거래 완료!"); location.href="/book/${bookId}";</script>`);
+        if ( req.user.id !== theComment.UserId ) {
+            await Book.update({  // 거래 완료. 거래 완료 되면 화면에 뿌려주는 아이템들의 조건들이 다 바뀔 예정
+                sold: 1,
+                SoldId: UserId,
+            }, {
+                where: { id: theBook.id},
+            });                             // 거래 완료되면 판매내역으로 이동할지 조원들과 회의
+            return res.send(`<script type="text/javascript">alert("거래 완료!"); location.href="/book/${bookId}";</script>`);
+        } else {
+            return res.send(`<script type="text/javascript">alert("잘못된 접근 입니다.(자신한테 거래)"); location.href="/book/${bookId}";</script>`);
+        }
     } else {
         return res.send(`<script type="text/javascript">alert("판매자가 아닙니다."); location.href="/book/${bookId}";</script>`);
     }
