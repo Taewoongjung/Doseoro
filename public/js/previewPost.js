881094c5cfecfb733f26423ce8310d79ee1b04b7
Bhanjo <hanjo1515@naver.com>
2021-05-06 17:54:53 +0900

    0506_check photo limit

--- public/js/previewPost.js
+++ public/js/previewPost.js
@@ -1,4 +1,5 @@
 
+// 프리뷰
 function readImg(input) {
     if (input.files && input.files[0]) {
         // 파일 리더로 파일 읽는 역할
@@ -14,7 +15,21 @@ function readImg(input) {
     }
 }
 
+// 파일 개수 제한
+function checkLimit(num) {
+    const ckPhoto = document.getElementById('ckPhoto');
+    const registBtn = document.getElementById('registBtn');
+
+    if(num > 5) {
+        ckPhoto.innerHTML = '<p class="text-danger fw-bold">사진은 최대 5개 까지 등록 가능합니다!</p>';
+        registBtn.disabled = 'disabled';
+    } else {
+        ckPhoto.removeChild(ckPhoto.childNodes[0]);
+        registBtn.disabled = false;
+    }
+}
 const inputImg = document.getElementById('book-photo');
 inputImg.addEventListener("change", e => {
-    readImg(e.target)
+    readImg(e.target);
+    checkLimit(inputImg.files.length);
 })
