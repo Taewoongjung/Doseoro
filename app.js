96a3b48703fc616e5c4c6a4eded6df0602c59114
Taewoong <aipooh8882@naver.com>
2021-05-07 23:24:00 +0900

    2021-05-07

--- app.js
+++ app.js
@@ -102,7 +102,7 @@ app.use((err, req, res, next) => {
   res.locals.message = err.message;
   res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error');{{notice.id}}
 });
 
 // const server = app.listen(app.get('port'), () => {
