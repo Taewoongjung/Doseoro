1bbf383eb748fa6bf70da200f107bc5b8d3d580d
Taewoong <aipooh8882@naver.com>
2021-05-12 22:46:01 +0900

    '로그인이 필요합니다' 라는 alert창이 뜨면 로그인 창으로 리다이렉트 되는걸로 고침

--- routes/middlewares.js
+++ routes/middlewares.js
@@ -2,7 +2,7 @@ exports.isLoggedIn = (req, res, next) => {
     if (req.isAuthenticated()) {
         next();
     } else {
-        res.send(`<script type="text/javascript">alert("로그인이 필요합니다."); location.href="/";</script>`);
+        res.send(`<script type="text/javascript">alert("로그인이 필요합니다."); location.href="/login";</script>`);
         
     }
 };
