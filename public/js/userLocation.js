2e477387174f76185b09d0c8ab13dafbf8511ca9
Bhanjo <hanjo1515@naver.com>
2021-05-12 17:15:30 +0900

    0512_location button design

--- public/js/userLocation.js
+++ public/js/userLocation.js
@@ -86,6 +86,8 @@ function displayCenterInfo(result, status) {
                 console.log("getInfo3 = ", result[i].region_3depth_name);
                 console.log("getInfoAll = ", result[i].address_name);
                 alert("'위치설정하기' 버튼을 꼭 눌러주세요!");
+                const submitBtn = document.getElementById('submitBtn');
+                submitBtn.style.display = "block";
 
                 redirection(getInfo.value);
                 return
