6b558a8201b70fe930cb5315c2927f2bbd342968
Bhanjo <hanjo1515@naver.com>
2021-05-16 00:49:32 +0900

    0516_pagination all board

--- public/js/movePage.js
+++ public/js/movePage.js
@@ -1,9 +1,41 @@
-function movePages() {
+function movePagesCommu() {
     const getPage = document.getElementById('inputPage').value;
     const maxPage = document.getElementById('maxPage').value;
     let addr = '/pages/community?page='
 
-    console.log(getPage + " / " + maxPage);
+    if(getPage > maxPage || getPage < 1)
+        alert("번호를 재확인하세요");
+    else
+        window.location.href = addr.concat(getPage);
+}
+
+function movePagesSale() {
+    const getPage = document.getElementById('inputPage').value;
+    const maxPage = document.getElementById('maxPage').value;
+    let addr = '/pages/saleBoard?page='
+
+    if(getPage > maxPage || getPage < 1)
+        alert("번호를 재확인하세요");
+    else
+        window.location.href = addr.concat(getPage);
+}
+
+function movePagesRequest() {
+    const getPage = document.getElementById('inputPage').value;
+    const maxPage = document.getElementById('maxPage').value;
+    let addr = '/pages/bookRequest?page='
+
+    if(getPage > maxPage || getPage < 1)
+        alert("번호를 재확인하세요");
+    else
+        window.location.href = addr.concat(getPage);
+}
+
+function movePagesFree() {
+    const getPage = document.getElementById('inputPage').value;
+    const maxPage = document.getElementById('maxPage').value;
+    let addr = '/pages/donationBoard?page='
+
     if(getPage > maxPage || getPage < 1)
         alert("번호를 재확인하세요");
     else
