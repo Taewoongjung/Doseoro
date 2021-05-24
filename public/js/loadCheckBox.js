9709cd2dba231294365c1926204743c16e5f6961
Bhanjo <hanjo1515@naver.com>
2021-05-24 18:56:05 +0900

    0524_책 상태 값 불러오기 구현

--- public/js/loadCheckBox.js
+++ public/js/loadCheckBox.js
@@ -1,4 +1,5 @@
-function loadCheck() {
+// 카테고리 불러오기
+function loadCheckCate() {
     const getCate = document.getElementById('getCate').value;
     let strSplit = getCate.split(','); // 카테고리 목록을 ',' 단위로 나눔
 
@@ -7,16 +8,36 @@ function loadCheck() {
     const cateVal = [];
     for(let i = 1; i < 11; i++) {
         cateArr[i] = document.getElementById('book-category' + i);
-        // console.log(cateArr[i]);
         cateVal[i] = document.getElementById('book-category' + i).value;
-        // console.log(cateVal[i]);
 
         // strSplit값과 카테고리 값을 비교
         for(let j = 0; j < 3; j++) {
-            if(strSplit[j] == cateVal[i]) {
-                // console.log("맞습니다");
+            if(strSplit[j] == cateVal[i])
                 cateArr[i].checked = true;
-            }
         }
     }
-}loadCheck();
+}
+
+// 책 상태 불러오기
+function loadCheckState() {
+    const getState = document.getElementById('getState').value;
+    let strSplit = getState.split(',');
+    let strSplitLen = strSplit.length;
+
+    const stateArr = [];
+    const stateVal = [];
+    for(let i = 1; i <= 5; i++) {
+        stateArr[i] = document.getElementById('book-state' + i);
+        stateVal[i] = document.getElementById('book-state' + i).value;
+
+        for(let j = 0; j < strSplitLen; j++) {
+            if(strSplit[j] == stateVal[i])
+                stateArr[i].checked = true;
+        }
+    }
+}
+
+function init() {
+    loadCheckCate();
+    loadCheckState();
+}init();
