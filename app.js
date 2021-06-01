56f9bfdc53fb1dafb599f599dceb823362aff30b
Taewoong <aipooh8882@naver.com>
2021-06-01 23:36:17 +0900

    클릭재킹 보호 코드, 브라우저 파일형식 추측 금지코드 추가

--- app.js
+++ app.js
@@ -54,6 +54,8 @@ if (process.env.NODE_ENV === 'production') {
   app.enable('trust proxy');
   app.use(morgan('combined'));
   app.use(helmet({ contentSecurityPolicy: false }));
+  app.use(helmet.noSniff());
+  app.use(helmet.frameguard("deny"));
   app.use(hpp());
 } else {
   app.use(morgan('dev'));
@@ -108,7 +110,7 @@ app.use((err, req, res, next) => {
   res.status(err.status || 500);
   res.render('error');
 });
-// a
+
 // const server = app.listen(app.get('port'), () => {
 //   console.log(app.get('port'), '번 포트에서 대기중');
 // });
